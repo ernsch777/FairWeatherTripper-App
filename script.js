a834973b6625a7230ccfdc7597975b94d7586e87
ernsch777 <82405659+ernsch777@users.noreply.github.com>
2021-11-01 08:59:57 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let currentDay = 0;
+let destination = [];
+console.log(destination);
 let weather = {
     apiKey: "68219aab5bbb48fe97a4f322fc15c400",
     fetchWeather: async function (city) {
@@ -15,19 +18,17 @@ let weather = {
             alert("Could not locate desired city or coordinates. Please try to enter again.");
             throw new Error("can't fetch");
         } else {
-            const data = await response.json();
+            let data = await response.json();
             this.displayWeather(data);
         }
     },
 
 
     displayWeather: function (data) {
-        const { city_name } = data;
-        const { high_temp, low_temp, pop, rh, wind_spd, wind_gust_spd } = data.data[0];
+        let { city_name } = data;
+        let { high_temp, low_temp, pop, rh, wind_spd, wind_gust_spd } = data.data[0];
         //document.querySelector(".weather.loading").innerText = "Weather in " + city_name;content: "Loading...";
         document.querySelector(".city_name").innerText = "Weather in " + city_name;
-        // document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
-        // document.querySelector(".description").innerText = description;
         document.querySelector(".high").innerText = high_temp + "°F";
         document.querySelector(".low").innerText = low_temp + "°F";
         document.querySelector(".pop").innerText = "Chance of precipitation: " + pop + " %";
@@ -40,9 +41,24 @@ let weather = {
         this.fetchWeather(document.querySelector(".search-bar").value);
     }
 };
-////////// Two event listeners for a search button click and entering the data with the enter key
-document.querySelector(".search button").addEventListener("click", function () {
+//////////Generate a new trip when button is selected
+document.querySelector("#generate").addEventListener("click", function () {
+    console.log(destination);
     weather.search();
+    currentDay = 0;
+
+});
+////Two ways to enter the destination and number of days. Stores locations in 'destination' array until needed in generate step
+document.querySelector(".search button").addEventListener("click", function () {
+    if ((currentDay + Number(document.querySelector(".days").value)) > 16) {
+        alert("Accurate weather forecasts exist out to a maximum of 16 days. Please enter a number of days for this stop so the total trip is 16 days or less.");
+        throw new Error("User entered too many days. Forecasts exist out to a maximum of 16.");
+    }
+    for (let i = currentDay; i < (Number(document.querySelector(".days").value) + currentDay); i++) {
+        destination[i] = document.querySelector(".search-bar").value;
+    }
+    currentDay = currentDay + Number(document.querySelector(".days").value);
+    console.log(currentDay);
 });
 document
     .querySelector(".search-bar").addEventListener("keyup", function (event) {
@@ -50,5 +66,41 @@ document
             weather.search();
         }
     });
-////////// start with Denver as your default starting point
-weather.fetchWeather("Denver");
+////////// start with ISP location as default starting point and use Seattle as a backup on error
+fetch('https://extreme-ip-lookup.com/json/')
+    .then(res => res.json())
+    .then(response => {
+        weather.fetchWeather(response.city);
+    })
+    .catch((data, status) => {
+        weather.fetchWeather("Seattle");
+    })
+
+
+
+
+
+
+// let addToDoButton = document.getElementById('addToDo');
+// let toDoContainer = document.getElementById('toDoContainer');
+// let inputField = document.getElementById('inputField');
+
+// addToDoButton.addEventListener('click', function(){
+//     var paragraph = document.createElement('p');
+//     paragraph.classList.add('paragraph-styling');
+//     paragraph.innerText = inputField.value;
+//     toDoContainer.appendChild(paragraph);
+//     inputField.value = "";
+//     paragraph.addEventListener('click', function(){
+//         paragraph.style.textDecoration = "line-through";
+//     })
+//     paragraph.addEventListener('dblclick', function(){
+//         toDoContainer.removeChild(paragraph);
+//     })
+// })
+
+// <div class="container">
+// <input id="inputField" type="text"><button id="addToDo">+</button>
+// <div class="to-dos" id="toDoContainer">
+// </div>
+// </div>
